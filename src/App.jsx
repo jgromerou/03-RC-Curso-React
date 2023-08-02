26a62c74365c857bff4c743cc7a41fd59442cb80
Larrosa Nicolas Andres (Producto y Tecnologia) <nLarrosa@lanacion.com.ar>
2023-08-02 08:22:49 -0300

    Template Inicial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';;
+import './styles/index.css'
+
+
+import { Gimoji } from './Gimoji';
+
+
+function App() {
+
+
+  return (
+    <>
+      <Gimoji />
+    </>
+  )
+}
+
+export default App
